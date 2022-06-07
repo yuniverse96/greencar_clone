2ec731b1e68956b5ad3edf4b2900ed33f5093924
yuni <qlrqod026@naver.com>
2022-06-07 16:35:07 +0900

    header finished

---
+++ README.md
@@ -0,0 +1,1 @@
+# greencar_clone
---
+++ js/common.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+		'afterLoad': function () {
+			if (this.hasClass("test")){
+				$("#header").addClass("white");
+			}else{
+				$("#header").removeClass("white");
+			}
+		},	
+	});
+
+
+	$("#header .nav .menu .tabs > ul .tab_box").on("mouseover", function(){
+		$("#header .nav .menu .tabs > ul .tab_box").removeClass("on");
+		$("#header").addClass("on");
+        $(this).addClass("on")
+	});
+
+	$("#header").mouseleave(function () {
+		$("#header").removeClass("on");
+		$("#header .nav .menu .tabs > ul .tab_box").removeClass("on");
+	});
+
+	
+
+
+		
+
+});
