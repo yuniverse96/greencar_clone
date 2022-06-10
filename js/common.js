4ad0d9397521f4ced09243dbed8dac5e24af7f43
yuni <qlrqod026@naver.com>
2022-06-11 00:15:59 +0900

    fullpage custum

--- js/common.js
+++ js/common.js
@@ -5,9 +5,12 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		//options here
 		autoScrolling:true,
+		navigation: true,   
+		navigationPosition: 'right',
+		showActiveTooltip: true,
+		navigationTooltips: ['이동을 새로 그리다 그린카', '가까운 그린존', '경제적인 요금/쿠폰', '간편한 앱', '그린카 이용안내','이벤트','공지사항'],
 		scrollHorizontally: true,
 		'afterLoad': function () {
-			
 			if ($(this).hasClass("test")){
 				$("#header").addClass("white");
 			}else{
